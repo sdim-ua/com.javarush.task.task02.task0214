b5875129c240aa25f4d097a0a5e3a079c9651720
sdim-ua <ingersol@yandex.ua>
2018-12-19 15:57:04 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,20 @@
+public class MethodCall
+{
+   public static void main(String[] args)
+   {
+      int a = 5, b = 7;
+      int m = min(a, b);
+      System.out.println("Minimum is "+ m);
+   }
+
+   public static int min(int c, int d)
+   {
+      int m2;
+      if (c < d)
+           m2 = c;
+      else
+           m2 = d;
+
+      return m2;
+   }
+}
